e74380553bba268f3216bc5ff1dbd95ef9cb929c
Walter Behrnes <Walter.behrnes@gmail.com>
2018-01-16 23:37:38 -0800

    Renamed Config class to QuickShareConfig. added icons paths to be set in the config file. Added quick_share file type, removed __init__ method since this was not really needed for how we are using the class.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ Name: config.py
 Version: 1.0
 Author: Walter Behrnes
 Contact: walter.behrnes@gmail.com
-Last Modified: 102/08/2017
+Last Modified: 01/16/2018
 Description:
    config.py is a module used to configure quick_share
 Example:
@@ -14,12 +14,11 @@ Requires:
 
 # NATIVE
 import os
-import sys
 import getpass
 from os.path import expanduser
 
 
-class Config(object):
+class QuickShareConfig(object):
     """
     Config is essentially a repository for Global values used through out coding
     """
@@ -48,20 +47,20 @@ class Config(object):
 
     # specify the root folder where your code lives
     # this was setup to work on a network folder.
-    # the initial configuration was setup to use dropbox
+    # the initial configuration is setup to use dropbox located in the users home folder.
     network = '{0}/Dropbox'.format(user_home)
 
-    # code is a common code direcotry  where code and icon data for code lives
+    # code is a common code directory  where code and icon data for code lives
     # the initial configuration was setup to work from the network folder.
     # change this as needed.
     code = '{0}/coding'.format(network)
 
     # temp is where time files wll be stored.
     # This will be used by applications which need to store temporary files
-    # edit / createt this folder if needed.
+    # edit / create this folder if needed.
     temp = "{0}/tmp".format(root)
 
-    # this is the common shared direcotry all users can access.
+    # this is the common shared directory all users can access.
     # this was initially setup to use the network folder, change as needed.
     shared_temp = "{0}/shared".format(network)
 
@@ -73,5 +72,22 @@ class Config(object):
     # edit as needed.
     exclude = ["character"]
 
-    def __init__(self):
-        pass
+    # HERE WE ARE CONFIGURING THE ICONS USED BY THE GUI
+    icon_plus = os.path.join(icons, "Copy_Color2_16x16.png")
+    icon_minus = os.path.join(icons, "Add_Blue_16x16.png")
+    icon_remove = os.path.join(icons, "Delete_Blue_16x16.png")
+    icon_next = os.path.join(icons, "Right_Clear_Orange_128x128.png")
+    icon_mail = os.path.join(icons, "Send_24x24.png")
+    icon_person = os.path.join(icons, "PersonFolder_White_16x16.png")
+    icon_collection = os.path.join(icons, "CollectionFolder_Gray_16x16.png")
+    icon_tool = os.path.join(icons, "Tool_Gray_16x16x.png")
+    icon_get = os.path.join(icons, "Get_GreenArrow_32x32.png")
+    icon_check = os.path.join(icons, "check_box_down_transparent_16x16.png")
+    icon_uncheck = os.path.join(icons, "check_box_up_transparent_16x16.png")
+    icon_trash = os.path.join(icons, "trashcan_orange_22x22.png")
+    icon_bulb = os.path.join(icons, "Info_Bulb_24x24.png")
+
+    # CONFIGURE THE SHARE DATA TYPE. FOR HOUDINI WE ARE SETTING THIS TO .cpio
+    # QuickShare COULD EASILY BE MODIFIED TO WORK WITH OTHER 3D APPS THAT
+    # STORE THEIR COPY PASTE MEMORY TO DISK. (like Maya, or Nuke)
+    share_type = ".cpio"
